495695bb10480e8f3e893316fdabb97b35fe2bb1
Juan <jwladi@gmail.com>
2018-06-11 17:33:58 +0200

    [0.0.0] Initial commit

---
+++ Source/TFCDungeon/TFCDungeonGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "TFCDungeonGameMode.h"
+#include "TFCDungeonCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ATFCDungeonGameMode::ATFCDungeonGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
