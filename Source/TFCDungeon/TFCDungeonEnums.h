31176624b9ea25aca1c6a34a38d5df561cbe7656
Juan <jwladi@gmail.com>
2018-06-16 19:05:12 +0200

    [0.5] Enum de personajes y algunos assets dinámicos

---
+++ Source/TFCDungeon/TFCDungeonEnums.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+UENUM(BlueprintType)
+enum class ECharacter : uint8
+{
+	Char_FireBoy UMETA(DisplayName = "FireBoy"),
+	Char_WaterGirl UMETA(DisplayName = "WaterGirl")
+};
