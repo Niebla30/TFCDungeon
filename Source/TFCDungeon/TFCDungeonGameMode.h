64f21f25bcfb79330ed3d6819be4abd0f88369e2
RGV\Ricardo <ricardo.guzman.velasco@outlook.com>
2018-06-18 16:11:17 +0200

    [0.3] Trasbase de GameMode a GameState
    No funcionando la posesión de peones.

--- Source/TFCDungeon/TFCDungeonGameMode.h
+++ Source/TFCDungeon/TFCDungeonGameMode.h
@@ -10,15 +10,6 @@ UCLASS(minimalapi)
 class ATFCDungeonGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
-
-public:
-	ATFCDungeonGameMode();
-	virtual void BeginPlay() override;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Characters)
-		TSubclassOf<APawn> BoyPawn;
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Characters)
-		TSubclassOf<APawn> GirlPawn;
 };
 
 
