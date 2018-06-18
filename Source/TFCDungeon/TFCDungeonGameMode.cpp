3503db8426a8445c244b98f23a4b6bc2e30b2dab
RGV\Ricardo <ricardo.guzman.velasco@outlook.com>
2018-06-18 18:13:48 +0200

    [0.3] Paso de la selección de peón de vuelta a GameMode
    Sigue sin funcionar el peón invitado, pero sí el anfitrión.
    Problemas con la posición en algunas ejecuciones (coge 0,0,0).

--- Source/TFCDungeon/TFCDungeonGameMode.cpp
+++ Source/TFCDungeon/TFCDungeonGameMode.cpp
@@ -1,3 +1,51 @@
 // © 2018, Juan Montero Román & Ricardo Guzmán Velasco
 
 #include "TFCDungeonGameMode.h"
+#include "TFCDungeonCharacter.h"
+#include "Kismet/GameplayStatics.h"
+#include "Engine/World.h"
+#include "TFCDungeonEnums.h"
+#include "TFCDungeonGameInstance.h"
+#include "UObject/ConstructorHelpers.h"
+
+ATFCDungeonGameMode::ATFCDungeonGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Characters/FireBoy/FireBoy_Character"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		BoyPawn = PlayerPawnBPClass.Class;
+	}
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnGirlBPClass(TEXT("/Game/Characters/WaterGirl/WaterGirl_Character"));
+	if (PlayerPawnGirlBPClass.Class != NULL)
+	{
+		GirlPawn = PlayerPawnGirlBPClass.Class;
+	}
+}
+
+void ATFCDungeonGameMode::PostLogin(APlayerController * NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+
+	auto StartingPoint = NewPlayer->GetPawn()->GetActorTransform();
+	NewPlayer->GetPawn()->Destroy();
+	NewPlayer->UnPossess();
+
+	ACharacter* character = nullptr;
+	TSubclassOf<APawn> pawn;
+	switch (Cast<UTFCDungeonGameInstance>(GetWorld()->GetGameInstance())->GetOwningCharacter())
+	{
+	case ECharacter::Char_FireBoy:
+		if (BoyPawn != NULL)
+			pawn = BoyPawn;
+		break;
+	case ECharacter::Char_WaterGirl:
+		if (GirlPawn != NULL)
+			pawn = GirlPawn;
+		break;
+	default:
+		break;
+	}
+	character = GetWorld()->SpawnActor<ACharacter>(pawn, StartingPoint.GetLocation(), FRotator(0, 0, 0));
+	NewPlayer->Possess(character);
+}
--- Source/TFCDungeon/TFCDungeonGameMode.h
+++ Source/TFCDungeon/TFCDungeonGameMode.h
@@ -10,6 +10,15 @@ UCLASS(minimalapi)
 class ATFCDungeonGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
+
+public:
+	ATFCDungeonGameMode();
+	virtual void PostLogin(APlayerController* NewPlayer) override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Characters)
+		TSubclassOf<APawn> BoyPawn;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Characters)
+		TSubclassOf<APawn> GirlPawn;
 };
 
 
