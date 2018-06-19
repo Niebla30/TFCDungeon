ec57473c1a0aa93959ca3eb043fc3a3be5be1d5e
Juan <jwladi@gmail.com>
2018-06-19 21:12:34 +0200

    [0.5] Probando conectividad.

--- Source/TFCDungeon/TFCDungeonEnums.h
+++ Source/TFCDungeon/TFCDungeonEnums.h
@@ -5,8 +5,9 @@
 UENUM(BlueprintType)
 enum class ECharacter : uint8
 {
-	Char_FireBoy UMETA(DisplayName = "FireBoy"),
-	Char_WaterGirl UMETA(DisplayName = "WaterGirl")
+	Char_FireBoy = 0 UMETA(DisplayName = "FireBoy"),
+	Char_WaterGirl UMETA(DisplayName = "WaterGirl"),
+	Char_MaxCharacters
 };
 
 UENUM(BlueprintType)
