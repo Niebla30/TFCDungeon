cd641e30416ec6222b0c906bdc45f0600923b424
Juan <jwladi@gmail.com>
2018-06-19 16:17:56 +0200

    [0.5] Delegate Estado del EndPoint

--- Source/TFCDungeon/TFCDungeonEnums.h
+++ Source/TFCDungeon/TFCDungeonEnums.h
@@ -8,3 +8,10 @@ enum class ECharacter : uint8
 	Char_FireBoy UMETA(DisplayName = "FireBoy"),
 	Char_WaterGirl UMETA(DisplayName = "WaterGirl")
 };
+
+UENUM(BlueprintType)
+enum class EEndPointState : uint8
+{
+	State_Busy UMETA(DisplayName = "Busy"),
+	State_Free UMETA(DisplayName = "Free")
+};
