af03b10968f3b8fcea70de2a39d19fe2c59735c7
Juan <jwladi@gmail.com>
2018-06-21 19:06:53 +0200

    [0.2] BP_Pool Basico, a falta de Behavior Component

--- Source/TFCDungeon/TFCDungeonEnums.h
+++ Source/TFCDungeon/TFCDungeonEnums.h
@@ -16,3 +16,11 @@ enum class EEndPointState : uint8
 	State_Busy UMETA(DisplayName = "Busy"),
 	State_Free UMETA(DisplayName = "Free")
 };
+
+UENUM(BlueprintType)
+enum class EElement : uint8
+{
+	Element_Fire UMETA(DisplayName = "Fire"),
+	Element_Water UMETA(DisplayName = "Water"),
+	Element_Poison UMETA(DisplayName = "Poison")
+};
