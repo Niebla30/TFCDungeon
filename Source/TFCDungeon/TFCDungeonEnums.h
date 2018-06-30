b57ad19245b24c8a0b71b262c8113eb879f70cf0
RGV\Ricardo <ricardo.guzman.velasco@outlook.com>
2018-06-30 22:43:31 +0200

    [0.6.1] Uso de la enumeración de elemento
    Problemas al mostrar las variables de tipo EElement en el editor.

--- Source/TFCDungeon/TFCDungeonEnums.h
+++ Source/TFCDungeon/TFCDungeonEnums.h
@@ -7,22 +7,22 @@
 UENUM(BlueprintType)
 enum class ECharacter : uint8
 {
-	Char_FireBoy = 0 UMETA(DisplayName = "FireBoy"),
-	Char_WaterGirl UMETA(DisplayName = "WaterGirl"),
-	Char_MaxCharacters
+	Char_FireBoy = 0	UMETA(DisplayName = "FireBoy"),
+	Char_WaterGirl		UMETA(DisplayName = "WaterGirl"),
+	Char_MaxCharacters	UMETA(Hidden)
 };
 
 UENUM(BlueprintType)
 enum class EEndPointState : uint8
 {
-	State_Busy UMETA(DisplayName = "Busy"),
-	State_Free UMETA(DisplayName = "Free")
+	State_Busy	UMETA(DisplayName = "Busy"),
+	State_Free	UMETA(DisplayName = "Free")
 };
 
-UENUM(BlueprintType)
+UENUM(BlueprintType, meta = (Bitflags))
 enum class EElement : uint8
 {
-	Element_None = 1<<0,
+	Element_None = 1<<0				UMETA(Hidden),
 	Element_Fire = 1<<1				UMETA(DisplayName = "Fire"),
 	Element_Water = 1<<2			UMETA(DisplayName = "Water"),
 	Element_Plasm = (1<<1) + (1<<2) UMETA(DisplayName = "Plasm"),
