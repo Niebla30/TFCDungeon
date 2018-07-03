818e595b77c8ebd1dbca167eb407874be9f25fb1
RGV\Ricardo <ricardo.guzman.velasco@outlook.com>
2018-07-03 19:01:00 +0200

    [0.6.1] Refactorización de elemento
    Descartado el uso de elementos en los EndPoints. La solución por personaje es suficiente.

--- Source/TFCDungeon/TFCDungeonEnums.h
+++ Source/TFCDungeon/TFCDungeonEnums.h
@@ -19,13 +19,11 @@ enum class EEndPointState : uint8
 	State_Free	UMETA(DisplayName = "Free")
 };
 
-UENUM(BlueprintType, meta = (Bitflags))
+UENUM(BlueprintType)
 enum class EElement : uint8
 {
-	Element_None = 1<<0				UMETA(Hidden),
-	Element_Fire = 1<<1				UMETA(DisplayName = "Fire"),
-	Element_Water = 1<<2			UMETA(DisplayName = "Water"),
-	Element_Plasm = (1<<1) + (1<<2) UMETA(DisplayName = "Plasm"),
-	Element_Poison = 1<<3			UMETA(DisplayName = "Poison")
+	Element_Fire	UMETA(DisplayName = "Fire"),
+	Element_Water	UMETA(DisplayName = "Water"),
+	Element_Plasm	UMETA(DisplayName = "Plasm"),
+	Element_Poison	UMETA(DisplayName = "Poison")
 };
-ENUM_CLASS_FLAGS(EElement)
